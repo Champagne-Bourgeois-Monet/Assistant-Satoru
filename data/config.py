e8476075fac4577f7c6cea4ea6fc742434bbe3fd
ALISHER KSHTYKENOV <alisher_kshtykenov@icloud.com>
2024-06-24 00:45:18 +0600

    Подключение и пробная проверка API Notion.

--- data/config.py
+++ data/config.py
@@ -1,11 +1,11 @@
-# BOT TOKEN моего telegram бота.
+# BOT TOKEN telegram бота.
 BOT_TOKEN = "6922327877:AAEw4qrZq5JWMM_S5cIg96MSqPYuJUlfVgs"
 
 # Id моего чата.
 admins = [
     1478600655,
-    # 1031212769,
-    # 1096326336
+    1031212769,
+    1096326336
 ]
 
 NOTION_TOKEN = "secret_L3Unfs7DJdjjq05eUBafn60V5SosDr6rUCC6k2rLZ8A"
